36abca4ff570ed3765ac14fb0b81c79049154b50
evanj0nes <48502664+evanj0@users.noreply.github.com>
2022-05-10 17:33:25 -0700

    Update publish_install.ps1

--- publish_install.ps1
+++ publish_install.ps1
@@ -1,11 +1,14 @@
 $vmDir = "src/vm";
 $asmDir = "src/asm";
+$installDir = [IO.Path]::Combine("$Env:UserProfile", "vm");
 
 try
 {
     Set-Location -Path $PSScriptRoot;
     Start-Process -FilePath "dotnet" -ArgumentList @("publish", $vmDir, "-c Release", "-r win10-x64", "-o ./publish/vm", "-p:PublishSingleFile=true", "-p:PublishTrimmed=true") -Wait;
-    Start-Process -FilePath "dotnet" -ArgumentList @("publish", $vmDir, "-c Release", "-r win10-x64", "-o ./publish/vm", "-p:PublishSingleFile=true", "-p:PublishTrimmed=true") -Wait;
+    Start-Process -FilePath "dotnet" -ArgumentList @("publish", $asmDir, "-c Release", "-r win10-x64", "-o ./publish/asm", "-p:PublishSingleFile=true", "-p:PublishTrimmed=true") -Wait;
+    Copy-Item -Path "./publish/vm/vm.exe" -Destination $installDir;
+    Copy-Item -Path "./publish/asm/asm.exe" -Destination $installDir;
 }
 catch 
 {
